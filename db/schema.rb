bf5bd952760fbb679a8cc16b417901dac1398ba5
David Celis <me@davidcel.is>
2017-04-30 18:48:32 -0700

    Add User model
    Signed-off-by: David Celis <me@davidcel.is>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501014526) do
+ActiveRecord::Schema.define(version: 20170501014819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,15 @@ ActiveRecord::Schema.define(version: 20170501014526) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "parent_category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
